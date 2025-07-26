4f42311e1ef19ef259c403a60d442bd2b2fd78d1
Merk48 <meryKassis48@gmail.com>
2025-07-26 19:27:13 +0300

    feat: adding project main page routing

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,7 +1,37 @@
 import { useState } from "react";
-const x = "";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "../features/menu/pages/Menu";
+import Cart from "../features/cart/pages/Cart";
+import Order from "../features/order/pages/Order";
+import CreateOrder from "../features/order/pages/CreateOrder";
+
+//#region Routing
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:id",
+    element: <Order />,
+  },
+]);
+//#endregion
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
